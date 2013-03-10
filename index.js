ef546f68407c87886c80245c4165b73219633c38
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-03-10 15:26:30 +0100

    Export functions in index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var exportLibs = function(exports /*libs to include */) {
+	var feature, i, len, libfeatures, libname, obj;
+	
+	for(i = 1, len = arguments.length; i < len; i++) {
+		libname = arguments[i];
+		libfeatures = require('./lib/' + libname + '.js');
+		obj = exports;
+		if (libfeatures.__namespace__ && libfeatures.__namespace__ != "") {
+			if (!obj.hasOwnProperty(libfeatures.__namespace__)) {
+				obj[libfeatures.__namespace__] = {};
+			}
+			obj = obj[libfeatures.__namespace__];
+		}
+		for(feature in libfeatures) {
+			if (feature == '__namespace__') {
+				continue;
+			}
+			if (obj.hasOwnProperty(feature)) {
+				throw new Error('Feature exists: ' + feature);
+			}
+			obj[feature] = libfeatures[feature];
+		}
+	}
+	return exports;
+};
+
+exportLibs(module.exports, 'distance-calculator', 'formatter', 'gpx-parser');
---
+++ tests/test_export_features.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+
+var gpsUtil = require('../index.js');
+
+var vows = require('vows');
+
+var expectedExportedFunctionNames = ['getDMSLatitude',
+			'getTotalDistance',
+			'toDMS',
+			'gpxParse',
+			'getDistance',
+			'toDD',
+			'getDMSLongitude',
+			'gpxParseFile'];
+
+vows.describe('Test suite for exported functionalities').addBatch({
+	'Exported function names' : function() {
+		functionNames =  Object.keys(gpsUtil);
+		functionNames.forEach(function(name) {
+			assert.equal(expectedExportedFunctionNames.indexOf(name) != -1,true, name + ' was not in the exptected function names (expectedExportedFunctionNames)');
+		});
+	}
+}).export(module);
