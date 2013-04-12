0c38fb95d50f07e9abf970335eb05ccd365e3291
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-04-12 22:30:45 +0200

    Handle response only when we got 200. Serve the test data locally to avoid broken tests due to communication error. Adding tests for case when the returned data is broken xml, soft 404 and other status than 200

--- tests/test_parse_gpx.js
+++ tests/test_parse_gpx.js
@@ -1,8 +1,41 @@
-
 var assert = require('assert');
 
 var gpx = require('../lib/gpx-parser.js');
 var vows = require('vows');
+
+/* For testing url based gpx data */
+var http = require('http');
+var fs = require('fs');
+var PORT = 8080;
+var HOST = '127.0.0.1';
+http.createServer(function(req, res) {
+	if(req.url == '/data.gpx') {
+		fs.readFile('./tests/data/data.gpx', function(err, data) {
+			if(err) {
+				throw err;
+			}
+			res.writeHead(200, {
+				'Content-Type' : 'text/xml'
+			});
+			res.end(data);
+		});
+	} else if(req.url == '/bad.gpx') {
+		res.writeHead(404, {
+			'Content-Type' : 'text/html'
+		});
+		res.end('Hey bad request!');
+	} else if (req.url == '/soft404.gpx') {
+		res.writeHead(200, {
+			'Content-Type' : 'text/xml'
+		});
+		res.end('This is a soft 404 which has 202 as status code');
+	} else if (req.url == '/broken.gpx') {
+		res.writeHead(200, {
+			'Content-Type' : 'text/xml'
+		});
+		res.end(['<gpx version="1.1">', '<metadata>', '<link href="connect.garmin.com">', '<text>Garmin Connect</text>', '</link>', '  <time>2013-03-02T15:40:32.000Z</time>', ' </metadata>', '<trk>', '<name>Untitled</name>', ' <trkseg>', '<trkpt lon="17.661922238767147" lat="59.19305333867669">', '<ele>69.4000015258789</ele>', '<time>2013-03-02T15:40:31.000Z</time>', '</trkpt>', '<trkpt lon="17.662122901529074" lat="59.192982176318765">', '<ele>69.5999984741211</ele>', ' <time>2013-03-02T15:40:38.000Z</time>', '</trkpt>', '</gpx>'].join());
+	}
+}).listen(PORT, HOST);
 
 vows.describe('Test suite for parsing gpx').addBatch({
 	'Parse broken gpx data' : {
@@ -19,21 +52,21 @@ vows.describe('Test suite for parsing gpx').addBatch({
 		},
 		'Should return an array of two tracking points' : function(err, result) {
 			assert.deepEqual(result, [{
-				distance: undefined,
-				speed: undefined,
+				distance : undefined,
+				speed : undefined,
 				lat : 59.19305333867669,
 				lng : 17.661922238767147,
 				ele : 69.4000015258789,
 				time : new Date('2013-03-02T15:40:31.000Z')
 			}, {
-				distance: undefined,
-				speed: undefined,
+				distance : undefined,
+				speed : undefined,
 				lat : 59.192982176318765,
 				lng : 17.662122901529074,
 				ele : 69.5999984741211,
 				time : new Date('2013-03-02T15:40:38.000Z')
 			}]);
-			
+
 		}
 	},
 	'Parse gpx file data.gpx' : {
@@ -42,15 +75,15 @@ vows.describe('Test suite for parsing gpx').addBatch({
 		},
 		'Should return an array of two tracking points' : function(err, result) {
 			assert.deepEqual(result, [{
-				distance: undefined,
-				speed: undefined,
+				distance : undefined,
+				speed : undefined,
 				lat : 59.19305333867669,
 				lng : 17.661922238767147,
 				ele : 69.4000015258789,
 				time : new Date('2013-03-02T15:40:31.000Z')
 			}, {
-				distance: undefined,
-				speed: undefined,
+				distance : undefined,
+				speed : undefined,
 				lat : 59.192982176318765,
 				lng : 17.662122901529074,
 				ele : 69.5999984741211,
@@ -58,34 +91,50 @@ vows.describe('Test suite for parsing gpx').addBatch({
 			}]);
 		}
 	},
-  'Parse gpx URL' : {
+	'Parse gpx URL' : {
 		'topic' : function() {
-			gpx.gpxParseURL('http://chamerling.github.io/gpx-stats/data/test/data.gpx', this.callback);
+			gpx.gpxParseURL('http://' + HOST + ':' + PORT + '/data.gpx', this.callback);
 		},
 		'Should return an array of two tracking points' : function(err, result) {
 			assert.deepEqual(result, [{
-				distance: undefined,
-				speed: undefined,
+				distance : undefined,
+				speed : undefined,
 				lat : 59.19305333867669,
 				lng : 17.661922238767147,
 				ele : 69.4000015258789,
 				time : new Date('2013-03-02T15:40:31.000Z')
 			}, {
-				distance: undefined,
-				speed: undefined,
+				distance : undefined,
+				speed : undefined,
 				lat : 59.192982176318765,
 				lng : 17.662122901529074,
 				ele : 69.5999984741211,
 				time : new Date('2013-03-02T15:40:38.000Z')
 			}]);
-		}    
-  },
-  'Parse bad gpx URL' : {
+		}
+	},
+	'Parse bad gpx URL' : {
 		'topic' : function() {
-			gpx.gpxParseURL('http://chamerling.github.io/gpx-stats/data/test/bad.gpx', this.callback);
+			gpx.gpxParseURL('http://' + HOST + ':' + PORT + '/bad.gpx', this.callback);
 		},
 		'Should return an error' : function(err, result) {
 			assert.equal(err != null, true)
 		}
-  }
+	},
+	'Parse bad gpx URL which returns a soft 404' : {
+		'topic' : function() {
+			gpx.gpxParseURL('http://' + HOST + ':' + PORT + '/soft404.gpx', this.callback);
+		},
+		'Should return an error' : function(err, result) {
+			assert.equal(err != null, true)
+		}
+	},
+	'Parse gpx URL which returns a broken xml' : {
+		'topic' : function() {
+			gpx.gpxParseURL('http://' + HOST + ':' + PORT + '/broken.gpx', this.callback);
+		},
+		'Should return an error' : function(err, result) {
+			assert.equal(err != null, true)
+		}
+	}
 }).export(module)
