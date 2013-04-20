132867578a1ddbaddbde1844c342dab997f5a83b
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-04-20 19:36:18 +0200

    Fix: seems that tcx file has entry that doesn't have any Extensions which cause crash

--- tests/test_parse_tcx.js
+++ tests/test_parse_tcx.js
@@ -86,6 +86,37 @@ vows.describe('Test suite for parsing tcx').addBatch({
 
 		}
 	},
+	'Parse tcx file data_missing_ext.tcx' : {
+		'topic' : function() {
+			tcx.tcxParseFile('./tests/data/data_missing_ext.tcx', this.callback);
+		},
+		'Should return an array of three tracking points' : function(err, result) {
+			assert.deepEqual(result, [{
+				distance : 0,
+				lat : null,
+				lng : null,
+				speed : 1.7480000257492068,
+				time : new Date('2013-01-16T18:54:46.000Z'),
+				altitude : 48.400001525878906
+			}, {
+				distance : 10.600000381469727,
+				lat : 59.19330538250506,
+				lng : 17.662933934479952,
+				speed : null,
+				time : new Date('2013-01-16T18:54:52.000Z'),
+				altitude : 68.4000015258789
+			}, {
+				distance : 29.540000915527344,
+				lat : 59.19347486458719,
+				lng : 17.662943825125694,
+				speed : 2.598000049591064,
+				time : new Date('2013-01-16T18:54:58.000Z'),
+				altitude : 68.4000015258789
+			}]);
+
+		}
+	},
+
 	'Parse tcx URL' : {
 		'topic' : function() {
 			tcx.tcxParseURL('http://' + HOST + ':' + PORT + '/data.tcx', this.callback);
