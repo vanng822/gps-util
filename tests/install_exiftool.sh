20d7dfea3902918d890a3a916260238087ee4db4
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-05-24 20:24:51 +0200

    Clean up file after install

--- tests/install_exiftool.sh
+++ tests/install_exiftool.sh
@@ -1,11 +1,19 @@
 
-wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-9.29.tar.gz
-tar -xzf Image-ExifTool-9.29.tar.gz
-cd Image-ExifTool-9.29
-perl Makefile.PL
+VERSION=9.29
 
+wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-${VERSION}.tar.gz
+tar -xzf Image-ExifTool-${VERSION}.tar.gz
+cd Image-ExifTool-${VERSION}
+## make & install
+perl Makefile.PL
 sudo make install
 
+## clean up
+cd ..
+rm Image-ExifTool-${VERSION}.tar.gz
+rm -r Image-ExifTool-${VERSION}
+
+## test if it works
 exiftool -ver
 
 res=$?
