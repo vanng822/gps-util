2525fd4ce1010fc4e0f49fcdcb3ba4a0197b1604
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-12-14 19:39:59 +0100

    Fix, incomplete test case name

--- tests/test_geohash.js
+++ tests/test_geohash.js
@@ -30,7 +30,7 @@ vows.describe('Test suite geohash').addBatch({
 		var result = geohash.geohashEncode(59.328930, 18.064910);
 		assert.equal(result, 'u6sce0d2nfjh');	
 	},
-	'Given u4ouydh I should get' : function() {
+	'Given u4ouydh I should get an Error' : function() {
 		assert.throws(function() {
 			geohash.geohashDecode('u4ouydh');
 		}, Error);
