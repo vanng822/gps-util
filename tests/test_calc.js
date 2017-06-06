3854b718aee2cdbed7fad67a5012089ecf38aa99
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2017-06-06 10:32:01 +0700

    TESTFIX, rename variable

--- tests/test_calc.js
+++ tests/test_calc.js
@@ -16,29 +16,29 @@ vows.describe('Test suite for calc').addBatch({
 			});
 		},
 		'expected result from calculateFromGPX' : function(err, result) {
-			let precision = 0.0000000000001;
+			let preci = 0.0000000000001;
 			assert.equal(err, null);
 			assert.equal(result.points.length, 20);
-			assert.ok(Math.abs(result.averageSpeed - 4.450676859448075) < precision);
-			assert.ok(Math.abs(result.fastestSpeed - 6.96311967571195) < precision);
-			assert.ok(Math.abs(result.slowestSpeed - 1.7153931748868099) < precision);
-			assert.ok(Math.abs(result.totalDistance -  396.11024049087865) < precision);
+			assert.ok(Math.abs(result.averageSpeed - 4.450676859448075) < preci);
+			assert.ok(Math.abs(result.fastestSpeed - 6.96311967571195) < preci);
+			assert.ok(Math.abs(result.slowestSpeed - 1.7153931748868099) < preci);
+			assert.ok(Math.abs(result.totalDistance -  396.11024049087865) < preci);
 			assert.equal(result.totalTime, 89);
 
 			let point2 = result.points[2];
-			assert.ok(Math.abs(point2.lat - 59.18158857151866) < precision);
-			assert.ok(Math.abs(point2.lng - 17.673767544329166) < precision);
-			assert.ok(Math.abs(point2.ele - 33.400001525878906) < precision);
-			assert.ok(Math.abs(point2.speed - 4.221760443523958) < precision);
-			assert.ok(Math.abs(point2.distance - 34.83194761671427) < precision);
+			assert.ok(Math.abs(point2.lat - 59.18158857151866) < preci);
+			assert.ok(Math.abs(point2.lng - 17.673767544329166) < preci);
+			assert.ok(Math.abs(point2.ele - 33.400001525878906) < preci);
+			assert.ok(Math.abs(point2.speed - 4.221760443523958) < preci);
+			assert.ok(Math.abs(point2.distance - 34.83194761671427) < preci);
 			assert.deepEqual(point2.time, new Date('2012-04-28T16:04:39.000Z'));
 
 			let point18 = result.points[18];
-			assert.ok(Math.abs(point18.lat - 59.182679979130626) < precision);
-			assert.ok(Math.abs(point18.lng - 17.67968793399632) < precision);
-			assert.ok(Math.abs(point18.ele - 30.200000762939453) < precision);
-			assert.ok(Math.abs(point18.speed - 4.084766149733331) < precision);
-			assert.ok(Math.abs(point18.distance - 393.5258418687884) < precision);
+			assert.ok(Math.abs(point18.lat - 59.182679979130626) < preci);
+			assert.ok(Math.abs(point18.lng - 17.67968793399632) < preci);
+			assert.ok(Math.abs(point18.ele - 30.200000762939453) < preci);
+			assert.ok(Math.abs(point18.speed - 4.084766149733331) < preci);
+			assert.ok(Math.abs(point18.distance - 393.5258418687884) < preci);
 			assert.deepEqual(point18.time, new Date('2012-04-28T16:05:54.000Z'));
 		}
 	},
